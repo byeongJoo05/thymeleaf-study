92f2d9cc3f0e3fbd9201e48ff967a2af28374583
SonByeongJoo <thsqudwn05@gmail.com>
2024-02-11 18:07:01 +0900

    feat: SSR 컴포넌트 종속화
    - 재사용성을 고려한 분리는 어떻게 할 수 있을까.

---
+++ src/main/java/com/example/thymeleafstudy/controller/examController.java
@@ -0,0 +1,25 @@
+package com.example.thymeleafstudy.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/exam")
+@Log4j2
+public class examController {
+
+    @GetMapping("/view")
+    public String example1 (@RequestParam("param1") String param1,
+                            Model model) {
+
+        model.addAttribute("model1", "test1");
+        model.addAttribute("model2", "test2");
+        model.addAttribute("condition", 1);
+        return "main-view";
+    }
+}
